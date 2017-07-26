b0890806ea09756041c5307eead130bc5b0be22a
liji8162 <liji8162@mds-xa101s-p009.vdi.student.otago.ac.nz>
2017-07-26 19:50:04 +1200

    changed the type of collection of Products to HashMap. References #13.

--- sale/src/dao/ProductList.java
+++ sale/src/dao/ProductList.java
@@ -1,8 +1,9 @@
 package dao;
 
 import domain.Product;
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.TreeSet;
 
 /**
@@ -10,15 +11,22 @@ import java.util.TreeSet;
  * @author liji8162
  */
 public class ProductList {
-	/**Product should be implemented in an ArrayList because it allows duplicates*/
-	private static Collection<Product> products = new ArrayList<>();
+	/**Product should be implemented in a HashMap because we are able to use
+	 * unique identifier to avoid duplicates. 
+	 * A type of product has an unique ID instead of a product has an ID. 
+	 * when users typed a product that the system has already had,
+	 * then the product will not be saved*/
+	private static Map<Integer,Product> products = new HashMap<>();
 	/**categories should be implemented in a TreeSet collection because 
 	 * categories are unique and we expect them to be sorted*/
 	private static Collection<String> categories = new TreeSet<>();
 	
 	
 	public void addProduct(Product product){
-		products.add(product);
+		/**may implement codes to handle what's gonna happen 
+		 * when users try to save a product twice. 
+		 * getQuantity() and then set again*/
+		products.put(product.getId(),product);
 		String category = product.getCategory();
 		categories.add(category);
 		
@@ -30,7 +38,7 @@ public class ProductList {
 	}
 	
 	public Collection<Product> getProducts(){
-		return products;
+		return products.values();
 		
 	}
 			
