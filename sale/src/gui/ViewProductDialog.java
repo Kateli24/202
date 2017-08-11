275cb20cd6c97fe1a1a16e14e44f441db680a6b3
liji8162 <liji8162@mds-xa101s-p009.vdi.student.otago.ac.nz>
2017-08-11 14:11:13 +1200

    fixed the comments

--- sale/src/gui/ViewProductDialog.java
+++ sale/src/gui/ViewProductDialog.java
@@ -220,48 +220,6 @@ public class ViewProductDialog extends javax.swing.JDialog {
 		this.viewProductsModel.updateItems(productDao.productsByCategory(seletedCategory));
    }//GEN-LAST:event_comboFilterActionPerformed
 
-//	/**
-//	 * @param args the command line arguments
-//	 */
-//	public static void main(String args[]) {
-//		/* Set the Nimbus look and feel */
-//		//<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-//		/* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-//         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-//		 */
-//		try {
-//			for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-//				if ("Nimbus".equals(info.getName())) {
-//					javax.swing.UIManager.setLookAndFeel(info.getClassName());
-//					break;
-//				}
-//			}
-//		} catch (ClassNotFoundException ex) {
-//			java.util.logging.Logger.getLogger(ViewProductDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//		} catch (InstantiationException ex) {
-//			java.util.logging.Logger.getLogger(ViewProductDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//		} catch (IllegalAccessException ex) {
-//			java.util.logging.Logger.getLogger(ViewProductDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
-//			java.util.logging.Logger.getLogger(ViewProductDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//		}
-//		//</editor-fold>
-//
-//		/* Create and display the dialog */
-//		java.awt.EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				ViewProductDialog dialog = new ViewProductDialog(new javax.swing.JFrame(), true,productDao);
-//				dialog.addWindowListener(new java.awt.event.WindowAdapter() {
-//					@Override
-//					public void windowClosing(java.awt.event.WindowEvent e) {
-//						System.exit(0);
-//					}
-//				});
-//				dialog.setVisible(true);
-//			}
-//		});
-//	}
-
 	/*JList by default holds strings, went to properties to change the generics*/
    // Variables declaration - do not modify//GEN-BEGIN:variables
    private javax.swing.JButton buttonSearch;
