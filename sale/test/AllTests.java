ebc9448b65bdead4eccdb6c006b0cd5c7af0d765
liji8162 <liji8162@mds-xa101s-p009.vdi.student.otago.ac.nz>
2017-08-19 17:03:09 +1200

    implemented a AllTests class that can run all the tests. Closes #26.

---
+++ sale/test/AllTests.java
@@ -0,0 +1,25 @@
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author liji8162
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+dao.DaoTester.class,
+gui.ProductDialogTest.class})
+public class AllTests {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+}
