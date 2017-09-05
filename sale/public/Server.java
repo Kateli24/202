d482e93e826fe62086405c95745602e85024d8cb
liji8162 <liji8162@mds-xa101s-p009.vdi.student.otago.ac.nz>
2017-09-05 12:40:22 +1200

    make new class called Server

---
+++ sale/public/Server.java
@@ -0,0 +1,15 @@
+
+import org.jooby.Jooby;
+
+
+/**
+ *
+ * @author liji8162
+ */
+public class Server extends Jooby {
+	public static void main(String[] args) {
+		new Server().start();
+		
+	}
+	
+}
