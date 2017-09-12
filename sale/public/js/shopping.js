6c84922a8f0e5831ea6168c8b4023ad04f2fc337
liji8162 <liji8162@mds-xa101s-p009.vdi.student.otago.ac.nz>
2017-09-12 13:07:45 +1200

    made products show in the table.References issue #43. References issue #42

--- sale/public/js/shopping.js
+++ sale/public/js/shopping.js
@@ -1,2 +1,16 @@
+"use strict";
+
+/*create application, and load used modules*/
+var app = angular.module('ShoppingApp' , ['ngResource', 'ngStorage']);
+
+app.factory('productDAO', function ($resource){
+	return $resource('/api/products/:id');
+});
+
+app.controller('ProductController', function(productDAO){
+this.products = productDAO.query();
+});
+
+
 
 
