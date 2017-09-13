8bd4feb7709e433f9918a0319903101d2f84144d
liji8162 <liji8162@mds-xa101s-p009.vdi.student.otago.ac.nz>
2017-09-13 17:48:26 +1200

    fixed creating customer page is not showing problem.References issue #43. Created a new page called Signin

--- sale/public/js/shopping.js
+++ sale/public/js/shopping.js
@@ -13,6 +13,16 @@ app.factory('categoryDAO', function ($resource) {
 	return $resource('/api/categories/:cat');
 });
 
+/**factory for customer*/
+app.factory('registerDAO', function ($resource){
+	return $resource('/api/register');
+});
+
+/**factory for sign in*/
+app.factory('signInDAO', function($resource){
+	return $resource('api/customers/:userName');
+});
+
 /**controller for product*/
 app.controller('ProductController', function(productDAO, categoryDAO){
 this.products = productDAO.query();
@@ -22,17 +32,31 @@ this.selectCategory = function(selectedCat){
 };
 });
 
-/**factory for customer*/
-app.factory('registerDAO', function (){
-	return $resource('/api/register');
-});
-
 /**controller for customer*/
-app.controller('CustomerController', function (registerDAO) {
+app.controller('CustomerController', function (registerDAO, signInDAO,$sessionStorage,$window) {
 	this.registerCustomer = function (customer) {
-		alert("register customer");
-		//registerDAO.save(null,customer);
+		registerDAO.save(null,customer);
 		console.log(customer);
+	};
+	
+	this.signInMessage = "Please sign in to continue.";
+
+	let ctrl = this;
+	this.signIn = function (userName, password) {
+// get customer from web service
+		signInDAO.get({'userName': userName},
+// success
+				  function (customer) {
+// also store the retrieved customer
+					  $sessionStorage.customer = customer;
+// redirect to home
+					  $window.window.location.href = '.';
+				  },
+// fail
+				  function () {
+					  ctrl.signInMessage = 'Sign in failed. Please try again.';
+				  }
+		);
 	};
 });
 
