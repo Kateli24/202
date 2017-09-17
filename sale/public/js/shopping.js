632f8d93ccbb38c0205fdaf03959c9908a6be3e9
liji8162 <liji8162@mds-xa101s-p009.vdi.student.otago.ac.nz>
2017-09-17 19:14:45 +1200

    finished sequence diagram for viewing products on web page.Closes #47.

--- sale/public/js/shopping.js
+++ sale/public/js/shopping.js
@@ -1,15 +1,15 @@
 "use strict";
 
 /*create application, and load used modules*/
-var app = angular.module('ShoppingApp' , ['ngResource', 'ngStorage']);
+var app = angular.module('ShoppingApp', ['ngResource', 'ngStorage']);
 
 /**factory for products according to id*/
-app.factory('productDAO', function ($resource){
+app.factory('productDAO', function ($resource) {
 	return $resource('/api/products/:id');
 });
 
 /**factory for getting all the products*/
-app.factory('allProductDAO', function ($resource){
+app.factory('allProductDAO', function ($resource) {
 	return $resource('/api/products');
 });
 
@@ -19,34 +19,34 @@ app.factory('categoryDAO', function ($resource) {
 });
 
 /**factory for customer*/
-app.factory('registerDAO', function ($resource){
+app.factory('registerDAO', function ($resource) {
 	return $resource('/api/register');
 });
 
 /**factory for sign in*/
-app.factory('signInDAO', function($resource){
+app.factory('signInDAO', function ($resource) {
 	return $resource('/api/customers/:userName');
 });
 
 /**controller for product*/
-app.controller('ProductController', function(productDAO, categoryDAO, allProductDAO){
-this.products = productDAO.query();
-this.categories = categoryDAO.query();
-this.selectCategory = function(selectedCat){
-	this.products = categoryDAO.query({"cat": selectedCat});
-};
-this.allProducts = function(allPro){
-	this.products = allProductDAO.query({"all" : allPro});
+app.controller('ProductController', function (productDAO, categoryDAO, allProductDAO) {
+	this.products = productDAO.query();
+	this.categories = categoryDAO.query();
+	this.selectCategory = function (selectedCat) {
+		this.products = categoryDAO.query({"cat": selectedCat});
+	};
+	this.allProducts = function (allPro) {
+		this.products = allProductDAO.query({"all": allPro});
 	};
 });
 
 /**controller for customer*/
-app.controller('CustomerController', function (registerDAO,signInDAO,$sessionStorage,$window) {
+app.controller('CustomerController', function (registerDAO, signInDAO, $sessionStorage, $window) {
 	this.registerCustomer = function (customer) {
-		registerDAO.save(null,customer);
+		registerDAO.save(null, customer);
 		console.log(customer);
 	};
-	
+
 	this.signInMessage = "Please sign in to continue.";
 
 	let ctrl = this;
@@ -54,18 +54,16 @@ app.controller('CustomerController', function (registerDAO,signInDAO,$sessionSto
 // get customer from web service
 		signInDAO.get({'userName': userName},
 // success
-				  function (customer) {
-// also store the retrieved customer
-
-					  $sessionStorage.customer = customer;
-					  
+				  function (customer) {						 
+// also store the retrieved custome
+									 $sessionStorage.customer = customer;
 // redirect to home
-					  $window.window.location.href = '.';
-				  },
-// fail
+									 $window.window.location.href = '.';
+								 },
 				  function () {
 					  ctrl.signInMessage = 'Sign in failed. Please try again.';
 				  }
+
 		);
 	};
 });
